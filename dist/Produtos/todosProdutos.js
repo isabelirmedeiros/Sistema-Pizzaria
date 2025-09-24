e18cc260769fa44d7c459a51dbae1774cded2677
isabelirmedeiros <isabeli.rsmedeiros@gmail.com>
2025-09-23 23:39:50 -0300

    Update todosProdutos.js

--- dist/Produtos/todosProdutos.js
+++ dist/Produtos/todosProdutos.js
@@ -3,6 +3,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.todosProdutos = todosProdutos;
 const mostrarPizza_1 = require("../Produtos/Pizzas/mostrarPizza");
 const mostrarBebida_1 = require("./Bebidas/mostrarBebida");
+const mostrarSobremesa_1 = require("./Sobremesas/mostrarSobremesa");
 function todosProdutos() {
     console.log("");
     console.log("---PIZZAS---");
@@ -10,4 +11,5 @@ function todosProdutos() {
     console.log("\n---BEBIDAS---");
     (0, mostrarBebida_1.mostrarBebida)();
     console.log("\n---SOBREMESAS---");
+    (0, mostrarSobremesa_1.mostrarSobremesa)();
 }
